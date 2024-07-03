dc82cbfdf8f2b0968bf7eb781d91b229b14dc606
Onyedikachi Ozoani <ozohenrywk@gmail.com>
2024-07-03 19:15:13 +0100

    Finished the LoadMoreData implementation logic

--- src/components/load-more-data/index.jsx
+++ src/components/load-more-data/index.jsx
@@ -15,7 +15,7 @@ export default function LoadMoreData() {
 		try {
 			setLoading(true);
 			const response = await fetch(
-				`https://dummyjson.com/products?limit=10&skip=${
+				`https://dummyjson.com/products?limit=5&skip=${
 					count === 0 ? 0 : count * 20
 				}`
 			);
@@ -38,12 +38,12 @@ export default function LoadMoreData() {
 	}, [count]);
 
 	useEffect(() => {
-		if (products.length === 100) {
+		if (count === 3) {
 			setIsButtonDisabled(true);
 		} else {
 			setIsButtonDisabled(false);
 		}
-	}, [products]);
+	}, [count]);
 
 	// whenever component renders/rerenders give a feedback if products are being fetched
 	if (loading) {
@@ -67,6 +67,7 @@ export default function LoadMoreData() {
 
 				<div className="button-container">
 					<button
+						className="btn"
 						disabled={isButtonDisabled}
 						onClick={() => {
 							setCount(count + 1);
@@ -74,7 +75,7 @@ export default function LoadMoreData() {
 						Load more products
 					</button>
 					{isButtonDisabled ? (
-						<p>You have reached to 100 products</p>
+						<p>You have reached to {products.length} products</p>
 					) : null}
 				</div>
 			</div>
